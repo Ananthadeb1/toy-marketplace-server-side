84bd24a24c74549da5f919610f370ed26c2676ce
Anantha Debnath <ananthadebnath103@gmail.com>
2023-07-01 23:47:06 +0600

    mongodb conection setup and get operation to check

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
-// here are the middleware
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zcvxptr.mongodb.net/?retryWrites=true&w=majority`;
 
-app.use(cors())
-app.use(express)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const toysCollection = client.db("toyLand").collection("toys");
+
+
+    // get all data for Alltoy page 
+    app.get('/alltoys', async (req, res) => {
+        const query = {}
+        const alltoys = await toysCollection.find(query).toArray();
+        res.send(alltoys)
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-    res.send('Toy house Running successfully');
+    res.send('Pure spices Running successfully');
 })
 app.listen(port, () => {
-    console.log('listening to port ', port);
+    console.log('listening to  ', port);
 })
