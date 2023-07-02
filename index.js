07f47b6a1ff603135b6f47d32d2a434d6cf65978
Anantha Debnath <ananthadebnath103@gmail.com>
2023-07-03 01:33:15 +0600

    cors origin setup

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://b7a11-toy-marketplace-server-side-ananthadeb1-two.vercel.app/"
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zcvxptr.mongodb.net/?retryWrites=true&w=majority`;
