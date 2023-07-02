33b526c98cd4514f95f28e59adcb9927ebcc4142
Anantha Debnath <ananthadebnath103@gmail.com>
2023-07-03 01:07:10 +0600

    index changed

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Pure spices Running successfully");
+  res.send("toy is running");
 });
 app.listen(port, () => {
   console.log("listening to  ", port);
